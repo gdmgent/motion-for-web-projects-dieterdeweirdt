2b83aba86bcbff663190fddddf68f7e8f02fe42e
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2023-03-27 10:53:01 +0200

    nieuwe les en oefening

---
+++ week6-presentation/scripts/app.js
@@ -0,0 +1,12 @@
+
+// handle up/down scrollwheel on the scroller, as most folks don't have horizontal scroll
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
