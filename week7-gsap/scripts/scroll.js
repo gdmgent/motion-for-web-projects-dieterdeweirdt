1f71fd8976a4438dff95e652db501bf4eac50cef
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2023-04-24 14:45:24 +0200

    update scroll script

--- week7-gsap/scripts/scroll.js
+++ week7-gsap/scripts/scroll.js
@@ -15,15 +15,3 @@ articles.forEach(article => {
  });
 
 
- gsap.from('article:nth-child(odd) img', {
-    scrollTrigger: {
-        trigger: article,
-        toggleActions: "play reverse play reset",
-        start: "top 80%",
-        end: "bottom 20%",
-    },
-    duration: .5, 
-    x: -300, 
-    opacity: 0});
-
-
